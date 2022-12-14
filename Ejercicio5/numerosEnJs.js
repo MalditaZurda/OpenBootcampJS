4b55d32a4d4843233d39bc7928e12cb2ff0333d9
Agustin Leiria <leiria.a97@gmail.com>
2022-12-14 14:15:12 -0300

    Ejercicio5
    Ejercicio 5
    tema: Numeros en JS

---
+++ Ejercicio5/numerosEnJs.js
@@ -0,0 +1,11 @@
+let alturaEnCm = 184;
+
+let alturaEnM = 1.84;
+
+let pesoEnKg = 87.3;
+
+let alturaHaciaArriba = Math.ceil(alturaEnM);
+
+let pesoHaciaAbajo = Math.floor(pesoEnKg);
+
+let valorMaxJs = Number.MAX_VALUE === Number.MAX_VALUE + 1;
