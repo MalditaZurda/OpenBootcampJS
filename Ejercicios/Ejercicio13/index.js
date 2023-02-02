b748452dbc945dea5f10e4230e68e7981b922053
Agustin Leiria <leiria.a97@gmail.com>
2023-02-02 08:22:24 -0300

    ejercicio 13
    eslint

---
+++ Ejercicios/Ejercicio13/index.js
@@ -0,0 +1,18 @@
+const nombre = "Agustin"
+const apellido = "Leiria"
+const apodo = "Zurdo"
+
+const estudiante = `${nombre} ${apellido}`
+
+const estudianteMayus = estudiante.toUpperCase()
+const estudianteMinus = estudiante.toLowerCase()
+
+const numeroDeLetrasEstudiante = estudiante.length
+
+const primeraLetraNombre = nombre[0]
+
+const ultimaLetraApellido = apellido.slice(-1)
+
+const sinEspacios = estudiante.replace(/ /g, "")
+
+const estaIncluida = estudiante.includes(nombre)
--- sesion13-eslint/eslint-npm/index.js
+++ sesion13-eslint/eslint-npm/index.js
@@ -1,6 +1,15 @@
 /* eslint-disable */
-const nombre = 'nombre'
 
-/* eslint-enable */
-// eslint-disable-next-line no-unused-vars
+const nombre = "nombre"
+
 const nombre2 = "nombre-2"
+
+var error1 = "Pato"
+
+function paraque(){
+    console.log("mamamamamamamam")
+}
+
+var error2 = 23
+
+var error3 = "error final" 
